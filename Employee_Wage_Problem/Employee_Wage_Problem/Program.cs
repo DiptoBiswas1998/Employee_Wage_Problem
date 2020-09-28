e48f56c70d688db5a534551d2f1aefec57e909b7
Dipto <diptobiswas1998@gmail.com>
2020-09-28 21:12:17 +0530

    [Dipto] refactor: My first commit

---
+++ Employee_Wage_Problem/Employee_Wage_Problem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Employee_Wage_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome msg
+            Console.WriteLine("Welcome to the Employee Wage Problem!");
+        }
+    }
+}
