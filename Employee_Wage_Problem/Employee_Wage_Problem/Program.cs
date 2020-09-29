1b14715ab5eafecdd6adc6bf6ffe07b66e2af01c
Dipto <diptobiswas1998@gmail.com>
2020-09-30 00:30:46 +0530

    [Dipto] refactor: Modifying the code

--- Employee_Wage_Problem/Employee_Wage_Problem/Program.cs
+++ Employee_Wage_Problem/Employee_Wage_Problem/Program.cs
@@ -20,34 +20,28 @@ namespace Welcome
             int empWageTotal = 0;
             int workingHrs = 0;
             int day = 0;
-
             for (day = 0; day < NumOfWorkingDays && workingHrs < TotalNumOfHours; day++)
             {
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
-
                 switch (empCheck)
                 {
                     case IsFullTime:
                         empHr = 8;
-                        workingHrs += empHr;
                         break;
                     case IsPartTime:
                         empHr = 4;
-                        workingHrs += empHr;
                         break;
                     default:
                         empHr = 0;
-                        workingHrs += empHr;
                         break;
                 }
+                workingHrs += empHr;
                 empWage = empHr * EmpRatePerHour;
                 empWageTotal += empWage;
-                Console.WriteLine("Employee Wage = " + empWage + " on day " + (day + 1));
             }
             Console.WriteLine("Total days worked = " + day + " Total hours worked = " + workingHrs);
             Console.WriteLine("Monthly Employee Wage = " + empWageTotal);
-
         }
     }
 }
